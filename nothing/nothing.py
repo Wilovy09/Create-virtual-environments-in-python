26810f84a7cf873b3d2c2868854e9cc30210e2c9
Wilovy09 <SrWilovy09@gmail.com>
2022-10-27 22:07:28 -0500

    Create-virtual-environments-in-python

---
+++ nothing/nothing.py
@@ -0,0 +1,6 @@
+# The only purpose of this file is that in the repository it says language: python
+
+# El unico proposito de este archivo es que en el repositorio diga lenguaje: python
+# Si, uso google traductor para mis textos en inglés
+
+print("Virtual environments are the best")
